23560949899d3e8d9278a1b34d1898db85159da0
Iskender Samat <iskndrsmt@gmail.com>
2021-08-09 01:37:36 +0300

    checking

--- src/Contexts/ProductContext.jsx
+++ src/Contexts/ProductContext.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 
-const ProductContextProvider = () => {
+export const addProductContext = React.createContext();
+
+const INIT_STATE = {
+    products:[],
+}
+
+const reducer = (state=INIT_STATE, action) =>{
+    switch (action.type) {
+        case "GET_PRODUCTS":
+            return{
+                ...state,
+                products: action.payload 
+            }    
+        default:
+            return state;
+    }
+
+}
+
+
+const ProductContextProvider = ({children}) => {
+
+
+
     return (
         <div>
             
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../Components/Header/Header';
 import Home from '../Components/Home/Home';
+import ProductContextProvider from '../Contexts/ProductContext';
 
 const Routes = () => {
     return (
-        <BrowserRouter>
-            <Header/>
-            <Switch>
-                <Route exact path="/" component={Home} />
-            </Switch>
-        </BrowserRouter>
+        <ProductContextProvider>
+            <BrowserRouter>
+                <Header/>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </BrowserRouter>
+        </ProductContextProvider>
     );
 };
 
