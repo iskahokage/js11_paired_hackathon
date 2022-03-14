acf69ddc423502bd371e01cc6d5296d6ba126cee
Iskender Samat <iskndrsmt@gmail.com>
2022-03-15 00:56:35 +0600

    deployed on firebase

--- src/Helpers/Consts.js
+++ src/Helpers/Consts.js
@@ -1,8 +1,8 @@
-export const JSON_API = 'http://localhost:8000/bikes';
-export const JSON_API_USER = 'http://localhost:8000/users';
+export const JSON_API = 'https://iskahokage-json-server.herokuapp.com/bikes';
+export const AUTH_API = 'https://iskahokage-json-server.herokuapp.com//users';
 // export const AUTH_API_REG = 'https://intense-retreat-64750.herokuapp.com/auth/registration';
 // export const AUTH_API_LOGIN = 'https://intense-retreat-64750.herokuapp.com/auth/login';
-export const AUTH_API = 'https://bmxkg.herokuapp.com/api'
+// export const AUTH_API = 'https://bmxkg.herokuapp.com/api'
 
 // export const ACTIONS = {
 //     AUTH_SUCCESS: 'AUTH_SUCCESS',
